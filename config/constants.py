a896b044e076d6a431ecff9b6afe3a523e713a3b
jungseoik <acc6452@naver.com>
2025-04-25 10:04:35 +0900

    [feat] constant module add

--- config/constants.py
+++ config/constants.py
@@ -12,3 +12,17 @@ DET_MODEL_NAME = "PersonDet_v3.2.0.pt"
 # Gender Classification
 CLS_REPO_ID = "PIA-SPACE-LAB/PersonGenderCls_v3.2.0"
 CLS_MODEL_NAME = "PersonGenderCls_v3.2.0.pt"
+
+# Data preprocess
+AIHUB_DATASET_ROOT = "/home/piawsa6000/nas192/tmp/jsi/015.한국인재식별이미지/01.데이터"
+AIHUB_OUTPUT_DIR = "prepro_data/prepro_aihub"
+
+PETA_DATASET_ROOT = "/home/piawsa6000/nas192/tmp/jsi/PETA dataset"
+PETA_OUTPUT_DIR = "prepro_data/prepro_peta"
+VALID_EXT = {".png", ".jpg", ".jpeg", ".bmp"}
+
+# peta data prepro 태그 정의
+MALE_TAG = "personalMale"
+FEMALE_TAG = "personalFemale"
+LABEL_FILENAME = "Label.txt"
+SPLITS = [0.8, 0.1, 0.1]  # train, val, test
--- train.py
+++ train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
-model = YOLO("Gender_cls/ai_hub2/weights/last.pt")
+model = YOLO("yolov8m-cls.pt")
 model.train(cfg="config/gender_cls_aihub.yaml")
